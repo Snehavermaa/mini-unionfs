149fa1972fce317ee727f9fabb67ff080044aab8
Teesta <sarkarmou0610@gmail.com>
2026-03-30 00:03:26 +0530

    Update cow.h

--- cow.h
+++ cow.h
@@ -1,6 +1,10 @@
 #ifndef COW_H
 #define COW_H
 
-int handle_cow_open(const char *path, int flags);
+#include <fuse.h>
+
+int cow_open(const char *path, struct fuse_file_info *fi);
+int cow_write(const char *path, const char *buf, size_t size,
+              off_t offset, struct fuse_file_info *fi);
 
 #endif
